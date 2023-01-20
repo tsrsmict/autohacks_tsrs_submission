f1bc8010404be8449cd2ad639002d6116b72d561
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-01-21 00:44:58 +0530

    added the GPT3 **formula**

--- backend/utils/openai_utils.py
+++ backend/utils/openai_utils.py
@@ -13,3 +13,22 @@ def gen_response(email:str,mood:str):
     frequency_penalty=0.5,
     presence_penalty=0
     )
+
+# Is this a yes or a no mail?
+# If yes, generate a yes or a no question for a hypothetical responder.
+# Would you like going to the park on Friday?
+# Generate yes or no MCQ answer options for this (Generate it in the form of a python list).
+# ['Yes', 'No']
+
+# What should be the tone of this email
+# Generate a list of 3-4 options for the tone of the email.
+# ['Happy', 'Sad', 'Angry', 'Neutral']
+
+# What should be the subject of this email
+# Generate a list of 3-4 options for the subject of the email.
+# ['Happy', 'Sad', 'Angry', 'Neutral']
+
+# What is the setting of the email
+# ['Work', 'Home', 'School', 'Other']
+
+#Generate a sample response to this email based on the above parameters.
