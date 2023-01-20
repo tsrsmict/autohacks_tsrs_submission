1b9909ae2a7f8953644c46081ba3ed2fd051985c
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-01-20 23:47:58 +0530

    wokr

--- backend/utils/openai_utils.py
+++ backend/utils/openai_utils.py
@@ -1,1 +1,15 @@
 import openai
+
+import config as cfg
+openai.api_key = cfg.OPEN_AI_KEY
+
+def gen_response(email:str,mood:str):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt= email+"stiff",
+    temperature=0.6,
+    max_tokens=500,
+    top_p=1,
+    frequency_penalty=0.5,
+    presence_penalty=0
+    )
