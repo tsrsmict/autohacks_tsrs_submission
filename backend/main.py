4fc9279cbd44947b1c14dda5e693dfb019a04168
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2023-01-21 13:08:41 +0530

    fixed

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from functions.login import submit_form
 from functions.signup import signup_form
+from utils.emai_check import check_email
 app = Flask(__name__)
 
 @app.route('/submitform', methods=['POST'])
@@ -11,6 +12,10 @@ def submitform():
 @app.route('/signup', methods=['POST'])
 def signupform_():
     var = signup_form()
+    return var
+@app.route('/checkemail', methods=['POST'])
+def checkemail():
+    var = check_email()
     return var
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
