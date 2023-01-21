d45528e6d3882581dbde94cad65289356257d532
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-01-21 15:16:40 +0530

    added extension support

---
+++ frontend-ext/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.createElement("div");
+rootElement.id = "react-chrome-app";
+document.body.appendChild(rootElement);
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
