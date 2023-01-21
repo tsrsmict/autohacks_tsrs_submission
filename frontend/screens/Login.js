ec1def0b63b6ef2c768c16b7fda7da5a09952383
shreedhar-raj <shreedharraj.buisness@gmail.com>
2023-01-21 13:05:42 +0530

    login

--- frontend/screens/Login.js
+++ frontend/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button, SafeAreaView } from 'react-native';
 import axios from 'axios';
 
 const FormScreen = () => {
@@ -20,16 +20,58 @@ const FormScreen = () => {
   };
 
   return (
-    <View>
-      <Text>Name:</Text>
-      <TextInput value={name} onChangeText={setName} />
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.heading}>Login</Text>
+        </View>
+        <View style={styles.formWrap}>
+        <View style={styles.formContainer}>
+          <Text>Name:</Text>
+          <TextInput style={styles.textInput} value={name} onChangeText={setName} />
+        </View>
 
-      <Text>Email:</Text>
-      <TextInput value={email} onChangeText={setEmail} />
+        <View style={styles.formContainer}>
+          <Text>Email:</Text>
+          <TextInput style={styles.textInput} value={email} onChangeText={setEmail} />
+        </View>
 
-      <Button title="Submit" onPress={handleSubmit} />
-    </View>
+        <Button style={styles.submit} title="Submit" onPress={handleSubmit} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default FormScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    margin: 20,
+    maxHeight: 'screen',
+  },
+  textInput: {
+    marginLeft: 10,
+    borderColor: '#0000',
+
+  },
+  formContainer: {
+      flexDirection: 'row',
+      margin: 15,
+  },
+  submit: {
+    height: 10,
+    width: 60,
+
+  },
+  formWrap: {
+    top: 200,
+  },
+  heading: {
+    top: 10,
+    fontSize: 35,
+    fontWeight: "bold",
+},
+
+});
