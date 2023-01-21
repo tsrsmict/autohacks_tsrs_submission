fd02af3069db7df9ff57d3fcdfa6573717734e5a
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2023-01-22 00:42:26 +0530

    checkemail fixed

--- frontend-ext/src/Components/Login.js
+++ frontend-ext/src/Components/Login.js
@@ -1,19 +1,50 @@
 import React from 'react'
-
+import { useState } from 'react'
+import axios from 'axios'
 function Login() {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/submitform", {
+        email,
+        pwd,
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className=" flex h-screen justify-center items-center">
-        <div className=''>
-        <h1>Already Have An Account? Login!</h1>
+        <div>
+        <div className="flex-col justify-center items-center bg-black px-20 pt-8 pb-24 rounded-lg text-white">
+          <h2 className='font-bold text-6xl m-16 text-center'>
+            Login
+          </h2>
+          <form className='text-white w-96'>
+            <div className="flex flex-col">
+              <label className='text-2xl font-bold' htmlFor="email">Email</label>
+              <input className='bg-gray-300 rounded-md p-2 mt-5' name='email' id='email' value={email} type={'text'} onChange={(e) => setEmail(e.target.value)}/>
+              </div>
+              <div className="flex flex-col mt-12">
+              <label className='text-2xl font-bold' htmlFor="pwd">Password</label>
+              <input className='bg-gray-300 rounded-md p-2 mt-5' name="pwd" value={pwd} type={'password'} onChange={(e)=> setPwd(e.target.value)}/>
+              </div>
+          </form>
 
-        <button className="ml-20 mt-2 bottom-20 bg-black hover:bg-white border border-2 border-black hover:text-black text-white font-bold py-2 px-4 rounded">
-    Login
-  </button>
+        <h1 className='text-center p-5'>Don't have an account? <a href='/signup'>Sign Up!</a></h1>
+        <div className='items-center text-center'>
+        <button onClick={(e) => handleSubmit(e)} className="mx-auto mt-2 bg-stone-300 hover:shadow-stone-400 bottom-20 hover:bg-white text-black font-bold py-2 px-5 mt-8 rounded">Login</button>
+  </div>
+  </div>
   </div>
 
   
   </div>
   )
 }
+
 
 export default Login
--- frontend-ext/src/Components/Signup.js
+++ frontend-ext/src/Components/Signup.js
@@ -1,15 +1,56 @@
 import React from 'react'
-import GoogleLogin from './google_utils'
+import { useState } from 'react'
+import axios from 'axios'
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [confirm, setConfirm] = useState("");
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/signupform", {
+        email,
+        pwd,
+        confirm
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className=" flex h-screen justify-center items-center">
-        <div className=''>
-            <GoogleLogin />
-            <h1>Already have an account? Login!</h1>
-            <button className="mt-2 ml-20 bottom-20 bg-black hover:bg-white border border-2 border-black hover:text-black text-white font-bold py-2 px-4 rounded">Signup</button>
-        </div>
+        <div>
+        <div className="flex-col justify-center items-center bg-black px-20 pt-8 pb-24 rounded-lg text-white">
+          <h2 className='font-bold text-6xl m-16 text-center'>
+            Sign Up
+          </h2>
+          <form className='text-white w-96'>
+            <div className="flex flex-col">
+              <label className='text-2xl font-bold' htmlFor="email">Email</label>
+              <input className='bg-gray-300 rounded-md p-2 mt-5' name='email' id='email' value={email} type={'text'} onChange={(e) => setEmail(e.target.value)}/>
+              </div>
+              <div className="flex flex-col mt-12">
+              <label className='text-2xl font-bold' htmlFor="pwd">Password</label>
+              <input className='bg-gray-300 rounded-md p-2 mt-5' name="pwd" value={pwd} type={'password'} onChange={(e)=> setPwd(e.target.value)}/>
+              </div>
+              <div className="flex flex-col mt-12">
+              <label className='text-2xl font-bold' htmlFor="pwd">Confirm Password</label>
+              <input className='bg-gray-300 rounded-md p-2 mt-5' name="pwd" value={confirm} type={'password'} onChange={(e)=> setConfirm(e.target.value)}/>
+              </div>
+          </form>
+
+        <h1 className='text-center p-5'>Already Have An Account? <a href='/login'>Login!</a></h1>
+        <div className='items-center text-center'>
+        <button onClick={(e) => handleSubmit(e)} className="mx-auto mt-2 bg-stone-300 hover:shadow-stone-400 bottom-20 hover:bg-white text-black font-bold py-2 px-5 mt-8 rounded">Sign Up</button>
+  </div>
+  </div>
+  </div>
+
+  
   </div>
   )
 }
+
 
 export default Signup
