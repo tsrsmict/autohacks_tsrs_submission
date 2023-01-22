be257676bf45edaf8122d1a359ce78325a9319dc
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-01-22 11:58:27 +0530

    working now

--- frontend-ext/src/App.js
+++ frontend-ext/src/App.js
@@ -1,40 +1,22 @@
-<<<<<<< HEAD
 import Home from "./Components/Home";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Link, Routes } from "react-router-dom";
 import Question from "./Components/Question";
 import Summary from "./Components/Summary";
-=======
-
-import Home from './Components/Home';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom"
-import Question from './Components/Question';
-import Summary from './Components/Summary';
-import EmailInput from './Components/EmailInput';
->>>>>>> 47ca45958ea83c41d781ce9d196d04180391e065
+import EmailInput from "./Components/EmailInput";
 
 function App() {
   return (
     <div>
       <HashRouter>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/question" element={<Question />} />
           <Route path="/summary" element={<Summary />} />
-=======
-        <Route path="/" element={<Home />} />
-         <Route path='/login' element={<Login />} />
-         <Route path='/signup' element={<Signup />} />
-         <Route path='/question' element={<Question />} />
-         <Route path='/summary' element={<Summary />} />
-         <Route path='/email' element={<EmailInput />}></Route>
->>>>>>> 47ca45958ea83c41d781ce9d196d04180391e065
+          <Route path="/email" element={<EmailInput />}></Route>
         </Routes>
       </HashRouter>
     </div>
