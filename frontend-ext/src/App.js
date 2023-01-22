11503e97b885f1782d0ae7750b4cd5780b673349
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-01-22 11:55:57 +0530

    router fixed

--- frontend-ext/src/App.js
+++ frontend-ext/src/App.js
@@ -1,25 +1,23 @@
-
-import Home from './Components/Home';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom"
-import Question from './Components/Question';
-import Summary from './Components/Summary';
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Question from "./Components/Question";
+import Summary from "./Components/Summary";
 
 function App() {
   return (
-      <div>
-      <BrowserRouter>
+    <div>
+      <HashRouter>
         <Routes>
-        <Route path="/" element={<Home />} />
-         <Route path='/login' element={<Login />} />
-         <Route path='/signup' element={<Signup />} />
-         <Route path='/question' element={<Question />} />
-         <Route path='/summary' element={<Summary />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/summary" element={<Summary />} />
         </Routes>
-      </BrowserRouter>
-      </div>
-      
+      </HashRouter>
+    </div>
   );
 }
 
