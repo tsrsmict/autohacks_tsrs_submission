76183d36fbe7ac2c83bf8e37200c46a892f8a43d
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2023-01-22 09:28:35 +0530

    email result fixed

--- backend/utils/emai_check.py
+++ backend/utils/emai_check.py
@@ -4,91 +4,96 @@ from email.header import decode_header
 import webbrowser
 import os
 
-# account credentials
+
 username = "aggarwalaarushprogrammer@gmail.com"
 password = "tfahygbfyclwffdm"
 
 imap_server = "imap.gmail.com"
 
-def clean(text):
-    # clean text for creating a folder
-    return "".join(c if c.isalnum() else "_" for c in text)
+emaildata = []
+def check_email():
+    # create an IMAP4 class with SSL 
+    imap = imaplib.IMAP4_SSL(imap_server)
+    # authenticate
+    imap.login(username, password)  
+    status, messages = imap.select("INBOX")
+    # number of top emails to fetch
+    N = 10
+    # total number of emails
+    messages = int(messages[0])
 
-# create an IMAP4 class with SSL 
-imap = imaplib.IMAP4_SSL(imap_server)
-# authenticate
-imap.login(username, password)  
-status, messages = imap.select("INBOX")
-# number of top emails to fetch
-N = 3
-# total number of emails
-messages = int(messages[0])
+    for i in range(messages, messages-N, -1):
+        # fetch the email message by ID
+        res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
+                # decode the email subject
+                subject, encoding = decode_header(msg["Subject"])[0]
+                if isinstance(subject, bytes):
+                    # if it's a bytes, decode to str
+                    subject = subject.decode(encoding)
+                # decode email sender
+                From, encoding = decode_header(msg.get("From"))[0]
+                if isinstance(From, bytes):
+                    From = From.decode('utf-8')
+                emailinfo = {
+                    "subject": subject,
+                    "from": From
+                }
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        # extract content type of email
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            # print text/plain emails and skip attachments
 
-for i in range(messages, messages-N, -1):
-    # fetch the email message by ID
-    res, msg = imap.fetch(str(i), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            # parse a bytes email into a message object
-            msg = email.message_from_bytes(response[1])
-            # decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                # if it's a bytes, decode to str
-                subject = subject.decode(encoding)
-            # decode email sender
-            From, encoding = decode_header(msg.get("From"))[0]
-            if isinstance(From, bytes):
-                From = From.decode(encoding)
-            print("Subject:", subject)
-            print("From:", From)
-            # if the email message is multipart
-            if msg.is_multipart():
-                # iterate over email parts
-                for part in msg.walk():
+                            emailinfo["email-body"] = body
+                        
+                else:
                     # extract content type of email
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
-                    except:
-                        pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # print text/plain emails and skip attachments
-                        print(body)
-                    elif "attachment" in content_disposition:
-                        # download attachment
-                        filename = part.get_filename()
-                        if filename:
-                            folder_name = clean(subject)
-                            if not os.path.isdir(folder_name):
-                                # make a folder for this email (named after the subject)
-                                os.mkdir(folder_name)
-                            filepath = os.path.join(folder_name, filename)
-                            # download attachment and save it
-                            open(filepath, "wb").write(part.get_payload(decode=True))
-            else:
-                # extract content type of email
-                content_type = msg.get_content_type()
-                # get the email body
-                body = msg.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    # print only text email parts
-                    print(body)
-            if content_type == "text/html":
-                # if it's HTML, create a new HTML file and open it in browser
-                folder_name = clean(subject)
-                if not os.path.isdir(folder_name):
-                    # make a folder for this email (named after the subject)
-                    os.mkdir(folder_name)
-                filename = "index.html"
-                filepath = os.path.join(folder_name, filename)
-                # write the file
-                open(filepath, "w").write(body)
-                # open in the default browser
-                webbrowser.open(filepath)
-            print("="*100)
-# close the connection and logout
-imap.close()
-imap.logout()
+                    content_type = msg.get_content_type()
+                    # get the email body
+                    body = msg.get_payload(decode=True).decode()
+                    if content_type == "text/plain":
+                        # print only text email parts
+                        emailinfo["email-body"] = body
+
+                
+                # print("="*100)
+        emaildata.append(emailinfo)
+    # close the connection and logout
+    imap.close()
+    imap.logout()
+    # print(emaildata)
+    y=1
+    for i in emaildata:
+        try:
+            body = i['email-body']
+            body.replace('\r', " ")
+        except:
+            i['email-body'] = ""
+        print(i)
+        x = i["from"].split(" <")
+        print(x)
+        i['from'] = x[0]
+        try: 
+            i['from-email'] = x[1]
+            i['from-email'] = i['from-email'].replace(">", "")
+        except:
+            emaildata.remove(i)
+
+        print(y)
+        y += 1
+
+
+check_email()
