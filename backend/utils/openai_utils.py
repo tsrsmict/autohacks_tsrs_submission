af51608ebad154a180468493184b9de173591bb3
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-01-22 09:51:21 +0530

    OPEN AI WORKS

--- backend/utils/openai_utils.py
+++ backend/utils/openai_utils.py
@@ -81,7 +81,7 @@ def gen_summary(email:list):
     emails = "\n\n".join(email)
     summary = openai.Completion.create(
     model="text-davinci-003",
-    prompt= email+ "\nGenerate a 100 word brief of the following emails",
+    prompt=  "\nGenerate a 100 word morning brief about the important stuff in the following emails. Address the person to whom the mails are written as you."+emails,
     temperature=0.6,
     max_tokens=500,
     top_p=1,
@@ -89,3 +89,5 @@ def gen_summary(email:list):
     presence_penalty=0
     )
     return summary.choices[0].text.strip()
+
+# print(gen_summary(["Hey John,\r\n\r\nJust a quick reminder that we are having a party next weekend at\r\nlocation], you're definitely on the guest list! Please let me know if\r\nyou're coming so I can make sure we have enough food and drinks.\r\n\r\nCan't wait to see you there!\r\n\r\nStacy\r\n","Dear John,\r\n\r\nI am writing to request your assistance with an order I recently placed\r\nwith your company. My order number is 09290822121 and it was placed on\r\n12/1/23. Unfortunately, I have run into some issues and I would like to\r\nrequest your help in resolving them.\r\n\r\nIf you could look into the matter and let me know what the next steps are,\r\nI would greatly\r\nappreciate it.\r\n\r\nThank you for your help and I look forward to hearing from you soon.\r\n\r\nSincerely,\r\nSmith\r\n","Dear All, We are excited to announce that the annual company picnic will be\r\nheld on Saturday, June 12th in Dubai. There will be food, games, and\r\nactivities for everyone to enjoy. Please RSVP by May 31st to\r\nshreedharraj.personal@gmail.com with the number of attendees from your\r\nteam. We look forward to seeing you all there! Best, Shreedhar Raj.\r\n","Hey John,\r\n\r\nLong time no see! How have you been? I was wondering if you're free for a\r\ncatch-up session over coffee sometime next week. Let me know what works for\r\nyou and we'll make it happen.\r\n\r\nTake care,\r\nRick\r\n"]))
